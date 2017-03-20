3521b454838e9f6b1d9e0711b34b088aa49aebe4
EtienneB <etienne.bernoux@edu.ece.fr>
2017-03-20 13:54:55 +0100

    exo3

---
+++ workspace/Lab6Lambda/src/fr/ece/bernoux/Lab6Lambda/Exercice3/NIi.java
@@ -0,0 +1,20 @@
+package fr.ece.bernoux.Lab6Lambda.Exercice3;
+
+import java.util.function.Consumer;
+/*
+public class NIi implements Seq {
+
+	public NIi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void forEach(Consumer<Object> consumer) {
+		
+		
+	}
+
+
+
+}*/
